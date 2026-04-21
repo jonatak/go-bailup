4f4b2a09ea9b0beb91a44afee82a3041d8afb736
Jonathan Billaud <jonathan.billaud@gmail.com>
2026-04-21 08:01:24 +0200

    feat: small simplification to apply changes on thermostats

--- internal/application/service.go
+++ internal/application/service.go
@@ -2,6 +2,8 @@ package application
 
 import "github.com/jonatak/go-bailup/internal/domain"
 
+type changeFunc = func(system *domain.HVACSystem) (domain.Change, error)
+
 type HVACService struct {
 	gateway HVACSystemGateway
 }
@@ -17,79 +19,39 @@ func (s *HVACService) CurrentState() (*domain.HVACSystem, error) {
 }
 
 func (s *HVACService) SetMode(mode domain.HVACSystemMode) (*domain.HVACSystem, error) {
-	system, err := s.gateway.GetHVACSystemState()
-	if err != nil {
-		return nil, err
-	}
-
-	change, err := system.SetMode(mode)
-	if err != nil {
-		return nil, err
-	}
-
-	return s.gateway.ApplyChange(change)
+	return s.executeChange(func(system *domain.HVACSystem) (domain.Change, error) {
+		return system.SetMode(mode)
+	})
 }
 
 func (s *HVACService) SetRoomPreset(
 	room string,
 	preset domain.ThermostatPreset,
 ) (*domain.HVACSystem, error) {
-	system, err := s.gateway.GetHVACSystemState()
-	if err != nil {
-		return nil, err
-	}
-
-	change, err := system.SetRoomPreset(room, preset)
-	if err != nil {
-		return nil, err
-	}
-
-	return s.gateway.ApplyChange(change)
+	return s.executeChange(func(system *domain.HVACSystem) (domain.Change, error) {
+		return system.SetRoomPreset(room, preset)
+	})
 }
 
 func (s *HVACService) TurnRoomOn(room string) (*domain.HVACSystem, error) {
-	system, err := s.gateway.GetHVACSystemState()
-	if err != nil {
-		return nil, err
-	}
-
-	change, err := system.TurnRoomOn(room)
-	if err != nil {
-		return nil, err
-	}
-
-	return s.gateway.ApplyChange(change)
+	return s.executeChange(func(system *domain.HVACSystem) (domain.Change, error) {
+		return system.TurnRoomOn(room)
+	})
 }
 
 func (s *HVACService) TurnRoomOff(room string) (*domain.HVACSystem, error) {
-	system, err := s.gateway.GetHVACSystemState()
-	if err != nil {
-		return nil, err
-	}
-
-	change, err := system.TurnRoomOff(room)
-	if err != nil {
-		return nil, err
-	}
-
-	return s.gateway.ApplyChange(change)
+	return s.executeChange(func(system *domain.HVACSystem) (domain.Change, error) {
+		return system.TurnRoomOff(room)
+	})
 }
 
 func (s *HVACService) SetCurrentSetpoint(
 	room string,
 	temp float64,
 ) (*domain.HVACSystem, error) {
-	system, err := s.gateway.GetHVACSystemState()
-	if err != nil {
-		return nil, err
-	}
-
-	change, err := system.SetCurrentSetpoint(room, temp)
-	if err != nil {
-		return nil, err
-	}
-
-	return s.gateway.ApplyChange(change)
+	return s.executeChange(func(system *domain.HVACSystem) (domain.Change, error) {
+		return system.SetCurrentSetpoint(room, temp)
+	})
 }
 
 func (s *HVACService) SetTemperature(
@@ -98,12 +60,18 @@ func (s *HVACService) SetTemperature(
 	preset domain.ThermostatPreset,
 	temp float64,
 ) (*domain.HVACSystem, error) {
+	return s.executeChange(func(system *domain.HVACSystem) (domain.Change, error) {
+		return system.SetTemperature(room, mode, preset, temp)
+	})
+}
+
+func (s *HVACService) executeChange(action changeFunc) (*domain.HVACSystem, error) {
 	system, err := s.gateway.GetHVACSystemState()
 	if err != nil {
 		return nil, err
 	}
 
-	change, err := system.SetTemperature(room, mode, preset, temp)
+	change, err := action(system)
 	if err != nil {
 		return nil, err
 	}
